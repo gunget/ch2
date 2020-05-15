d2a9cf13ea57c118624b11a360d5d0f780d6850b
KG <gunget@naver.com>
2020-05-15 15:02:06 +0900

    1. urlconf작성. 제네릭뷰 상속을 통한 view파일 작성

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
-# git push test
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
+
+#----list view : 북마크 테이블의 레코드리스트를 보여주라는 처리방식
+class BookmarkLV(ListView): #장고의 표준양식을 상속받아서 간단하게 리스트 표출
+    model = Bookmark #상속받았을때 필수적으로 넣어야 함.
+    ''' 함수형 뷰였을 경우에는 첫째, DB에서 쿼리를 통해 데이터를 가져와 객체1을 만들고
+    둘째, 딕셔너리 형태로 템플릿에서 사용하게 컨텍스트 변수(변수명:객체1 형식)를 만들고
+    셋째, render함수로 지정된 템플릿파일에 컨텍스트 변수를 적용시킨 HTML텍스트를 만들어서
+    넷째, 역시 render함수가 이를 HttpResponse객체에 담아 반환하는 작업을 한다. 
+    
+    제네릭 뷰가 이같은 과정을 축약해 위처럼 한줄의 코드로 만들어 버린것. 곧 자동으로 컨텍스트 변수는
+    object_list로, 템플릿파일은 모델명소문자_list.html(bookmark_list.html)로 지정된다.
+    '''
+
+#----Detail view : 북마크 테이블의 특정 레코드에 대한 상세정보를 보여주라는 처리방식
+class BookmarkDV(DetailView): #장고의 표준양식을 상속받아서 간단하게 리스트 표출
+    model = Bookmark
+    ''' 마찬가지로 컨텍스트 변수는 object로 템플릿파일은 모델명소문자_detail.html로 자동 지정됨
+    테이블에서 특정 객체를 가져오는 경우는(pk를 지정해서 호출할 경우) urlconf에서 넘어옴
+    '''
--- mainsite/urls.py
+++ mainsite/urls.py
@@ -13,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin #기본적으로 로딩. admin싸이트 접속용도
+from django.urls import path #기본적으로 로딩. path를 통해 보통은 접근
+from django.conf.urls import url #보통 프로젝트용 url과 앱용 url 두개가 있으나, 여기선 프로젝트용
+                                 #하나로만 바로 conf할꺼라 필요
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls), #장고 conf파일의 url함수는 내부적으로 re모듈을 호출하는 듯.
+                                      #url()의 첫번째 변수가 regex 즉 정규식임
+                                      #여기서 바로 정규식을 사용함. ^ ~로 시작하는.
+
+    #Class-based view for bookmark 앱
+    url(r'^bookmark/$', BookmarkLV.as_view, name='index'),# xx$ xx로 끝나야함
+    #원래는 view.view처리함수 형태가 되어야 하나, 장고의 제네릭(표준)뷰를 쓰기 위해 as_view 사용
+    #name은 이 url패턴을 뭐라고 부를 것인가를 정의하는 것
+    url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view, name='detail'),
+    #bookmark/11~/형태의 url. ?p<>는 정규식 그룹명을 지정한 것. 뷰를 호출할 때 primary key가
+    #변수로 같이 넘겨짐. views.detal(request, pk)처럼.
 ]
