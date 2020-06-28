a9c692a3403a28b7a6b42ae1b02056d82a4d15e9
KG <gunget@naver.com>
2020-06-28 13:27:06 +0900

    1. 앨범, 사진에 생성, 수정, 삭제 기능 추가하기  - 지워진 템플릿 파일 작성  - 버그 수정. 대소문자 처리에서도 오류 자주 발생

--- photo/views.py
+++ photo/views.py
@@ -29,6 +29,7 @@ class PhotoCreateView(LoginRequiredMixin, CreateView):
     #제출된 form의 유효성 검사를 마친 후 이상이 없으면 form_valid메소드를 호출 함(CreateView의 자동 기능)
 
 class PhotoChangeLV(LoginRequiredMixin, ListView):
+    model = Photo
     template_name = 'photo/photo_change_list.html'
 
     def get_queryset(self):#현재 로그인된 사용자가 owner로 되어 있는 리스트만 뽑아내 리스트로 보여줌
@@ -46,10 +47,11 @@ class PhotoDeleteView(OwnerOnlyMixin, DeleteView):
     success_url = reverse_lazy('photo:index')
 
 class AlbumChangeLV(LoginRequiredMixin, ListView):
+    model = Album
     template_name = 'photo/album_change_list.html'
 
     def get_queryset(self):
-        return Album.object.filter(owner=self.request.user)
+        return Album.objects.filter(owner=self.request.user)
 
 class AlbumDeleteView(OwnerOnlyMixin, DeleteView):
     model = Album
@@ -59,43 +61,69 @@ class AlbumDeleteView(OwnerOnlyMixin, DeleteView):
 from django.shortcuts import redirect #리다이렉트를 위한 숏컷 가져옴
 from photo.forms import PhotoInlineFormSet #인스턴스 가져옴
 
+class AlbumPhotoCV_revised(LoginRequiredMixin, CreateView):
+    model = Album
+    fields = ('name', 'description')
+    success_url = reverse_lazy('photo:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = PhotoInlineFormSet(self.request.POST, self.request.FILES)
+        else:
+            context['formset'] = PhotoInlineFormSet()
+        return context
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        context = self.get_context_data()
+        formset = context['formset']
+        for photoform in formset:
+            photoform.instance.owner = self.request.user
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.get_success_url())
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+
+
 #폼셋이란 동일한 폼 여러개로 구성된 폼을 지칭. 또 인라인 폼셋이란 메인 폼에 딸려있는 하위 폼셋을 지칭
 #테이블의 관계가 1:N일 경우, N테이블의 레코드 여러개를 동시에 입력받기 위한 용도로 인라인폼셋 사용.
 class AlbumPhotoCV(LoginRequiredMixin, CreateView):
     #createView:중요한 몇가지 클래스속성만 정의해주면 적절한 폼을 보여주고 입력된 내용의 유효성 검사를 한뒤
     #이상이 없으면 입력된 내용으로 테이블에 레코드를 지정
     model = Album
-    fields = ['name', 'description']
+    fields = ('name', 'description')
+    success_url = reverse_lazy('photo:index')
     template_name = 'photo/album_form.html'
 
-    '''
-     템플릿에 넘겨줄 context(변수)에 추가적인 데이터를 넣고 싶을 경우, 이를 재지정 하면 된다.
-     context는 기본적으로 딕셔너리이므로 딕셔너리 문법을 활용한다.
-        def get_context_data(self, **kwargs):
-            # 기본 구현을 호출해 context를 가져온다.
-            context = super(PublisherDetail, self).get_context_data(**kwargs)
-            # 모든 책을 쿼리한 집합을 context 객체에 추가한다.
-            context['book_list'] = Book.objects.all()
-            return context
-        처럼해서 추가적인 데이터를 넣을 수 있다(book_list항목을 넣어준 것) 
-    '''
-    ''' super().get_context_data(**kwargs)하면 호출되는 변수 값들. ListView를 상속받은 상태에서 불러온 예.
-    {'paginator': None, 'page_obj': None, 'is_paginated': False, 'object_list': <QuerySet 
-    [<Video: django 2.2>, <Video: django 1.11>]>, 'video_list': <QuerySet [<Video: django 2.2>, <Video: django 1.11>]>, 
-    'view': <videos.views.VideoListView object at 0x106292cc0>}
-    '''
+    #context의 확장을 위해 재정의
     def get_context_data(self, **kwargs): #context변수의 속성을 추가하기 위해 오버라이딩
         context = super().get_context_data(**kwargs)#부모것을 먼저 부른 뒤
-        if self.request.Post:
-            context['formset'] = PhotoInlineFormSet(self.request.Post, self.request.FILES)
+        if self.request.POST:
+            context['formset'] = PhotoInlineFormSet(self.request.POST, self.request.FILES)
             #formset이란 keyword에 request의 Post, FILE파라미터를 인라인 폼셋형태로 추가, context확장.
             #두 속성을 포함하여, 사진 폼셋을 앨범 context변수의 formset이란 속성에 인라인 폼셋형태로 지정한다.
             #하나의 앨범폼에 사진폼셋이 주르륵 붙어서 나타나도록 설정하기 위함인 듯
 
-            #보통 뷰는 return render(response, '.html', context)를 반환하는데, 이때 폼 뷰(edit view)의 경우
-            #context = {'form':form}으로 보내진다. 여기서 form은 form객체. 헌데 여기선 인라인폼셋을 사용,
-            #context ={'form':form, 'formset':PhotoIn~~}을 콘텍스트 변수로 템플릿에 반환하는 것으로 보인
-            #다. 따라서 템플릿에선 form과 formset을 변수명으로 다 사용할 수 있다.
+            '''
+             템플릿에 넘겨줄 context(변수)에 추가적인 데이터를 넣고 싶을 경우, 이를 재지정 하면 된다.
+             context는 기본적으로 딕셔너리이므로 딕셔너리 문법을 활용한다.
+                def get_context_data(self, **kwargs):
+                    # 기본 구현을 호출해 context를 가져온다.
+                    context = super(PublisherDetail, self).get_context_data(**kwargs)
+                    # 모든 책을 쿼리한 집합을 context 객체에 추가한다.
+                    context['book_list'] = Book.objects.all()
+                    return context
+                처럼해서 추가적인 데이터를 넣을 수 있다(book_list항목을 넣어준 것) 
+            '''
+            ''' super().get_context_data(**kwargs)하면 호출되는 변수 값들. ListView를 상속받은 상태에서 불러온 예.
+            {'paginator': None, 'page_obj': None, 'is_paginated': False, 'object_list': <QuerySet 
+            [<Video: django 2.2>, <Video: django 1.11>]>, 'video_list': <QuerySet [<Video: django 2.2>, <Video: django 1.11>]>, 
+            'view': <videos.views.VideoListView object at 0x106292cc0>}
+            '''
             '''
             Django uses request and response objects to pass state through the system.
             When a page is requested, Django creates an [HttpRequest object] that contains
@@ -107,6 +135,11 @@ class AlbumPhotoCV(LoginRequiredMixin, CreateView):
              어떤 request용 api는 장고의 미들웨어가 추가하기도 하는데, 대보적인게 Request.user라는 api.
             이는 장고의 AuthentificationMiddleware가 추가한다.
             '''
+
+            #보통 뷰는 return render(response, '.html', context)를 반환하는데, 이때 폼 뷰(edit view)의 경우
+            #context = {'form':form}으로 보내진다. 여기서 form은 form객체. 헌데 여기선 인라인폼셋을 사용,
+            #context ={'form':form, 'formset':PhotoIn~~}을 콘텍스트 변수로 템플릿에 반환하는 것으로 보인
+            #다. 따라서 템플릿에선 form과 formset을 변수명으로 다 사용할 수 있다.
         else: #request가 post가 아니면 인라인 폼셋에 데이터가 들어가있지 않은 형태로 context 확장
             context['formset'] = PhotoInlineFormSet()
         return context
@@ -139,8 +172,8 @@ class AlbumPhotoUV(OwnerOnlyMixin, UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        if self.request.Post:
-            context['formset'] = PhotoInlineFormSet(self.request.Post, self.request.FILES,
+        if self.request.POST:
+            context['formset'] = PhotoInlineFormSet(self.request.POST, self.request.FILES,
                                                     instance=self.object)
             #create때와 차이라면 특정 앨범을 지정한 뒤 수정하는 것이므로, 현재 앨범 객체를 instance로 지정해
             #줘야 하는 듯.
